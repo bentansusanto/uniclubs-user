7995e0f8f6cdfaa83180d59ada6fb69f51af1087
bentansusanto <tansusanto194@gmail.com>
2022-11-09 13:33:56 +0700

    update homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import CategorySection from './CategorySection'
 import HeroSection from './HeroSection'
 
 const Homepage = () => {
   return (
     <div>
       <HeroSection/>
+      <CategorySection/>
     </div>
   )
 }
